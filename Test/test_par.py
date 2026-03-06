0fe795cec2fce6d0cdc66181e08e02c168bc6d2a
Samuel Pineda y Kevin Martinez <dampineda62@gmail.com>
2026-03-06 12:25:03 -0500

    edit programa

--- Test/test_par.py
+++ Test/test_par.py
@@ -4,6 +4,6 @@ class TestPar(unittest.TestCase):
 	def Test_par(self):
 		self.assertTrue(es_par(4))
 	def Test_impar(self):
-	self.assertFalse(es_par(5))
+		self.assertFalse(es_par(5))
 if __name__== '__main__':
 	unittest.main()
