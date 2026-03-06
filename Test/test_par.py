c72784149507b27a6cde236c02dd2e675d6617c4
Samuel Pineda y Kevin Martinez <dampineda62@gmail.com>
2026-03-06 12:27:12 -0500

    edit programa 2.0

--- Test/test_par.py
+++ Test/test_par.py
@@ -1,9 +1,12 @@
 import unittes
 from Funciones.par import es_par
+
 class TestPar(unittest.TestCase):
-	def Test_par(self):
+	
+	def test_par(self):
 		self.assertTrue(es_par(4))
-	def Test_impar(self):
+	
+	def test_impar(self):
 		self.assertFalse(es_par(5))
-if __name__== '__main__':
+if __name__ == '__main__':
 	unittest.main()
