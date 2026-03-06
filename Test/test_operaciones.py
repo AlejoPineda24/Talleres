40cf9b2144d291f8df933e3bb48390c6a6cee225
Samuel Pineda y Kevin Martinez <dampineda62@gmail.com>
2026-03-06 12:14:24 -0500

    DOCUMENTO TALLER

--- Test/test_operaciones.py
+++
@@ -1,9 +0,0 @@
-import unittes
-from Funciones.par import es_par
-class TestPar(unittest.TestCase):
-	def Test_par(self):
-		self.assertTrue(es_par(4))
-	def Test_impar(self):
-	self.assertFalse(es_par(5))
-if __name__== '__main__':
-	unittest.main()
